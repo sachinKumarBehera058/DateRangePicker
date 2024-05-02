b91dd2bbd6a782f7233cbcb60f42dd71d4d2b9b6
Sachin Kumar Behera <sachin.behera@attryb.com>
2024-05-02 13:52:24 +0530

    prop passed from components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import CustomDateRangePicker from './components/CustomDateRangePicker/index';
+import moment from 'moment'; 
 
 function App() {
+
+  const currentDate = new Date();
+  const sevenDaysAgo = moment().subtract(7, 'days').toDate();
+  const [startDate, setStartDate] = useState(moment.utc(sevenDaysAgo).startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'));
+  const [endDate, setEndDate] = useState(moment.utc(currentDate).startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS[Z]'));
+
+  // Function to handle date range change
+  const handleDateRangeChange = (dateStr: string) => {
+    const dateRangeArray = dateStr.split(" - ");
+    let start = moment.utc(dateRangeArray[0]).startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+    let end = moment.utc(dateRangeArray[1]).startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   return (
     <>
-      <CustomDateRangePicker />
+      <CustomDateRangePicker
+        defaultStartDate={startDate}
+        defaultEndDate={endDate}
+        onDateRangeChange={handleDateRangeChange}
+      />
     </>
   );
 }
